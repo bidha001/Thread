e352c2a1802e8820a11e9fb17012b697830543a7
bidha001 <binitd@stud.ntnu.no>
2025-02-11 21:44:17 +0100

    added send request method to handle communication with the server

--- src/main/java/MultiClient.java
+++ src/main/java/MultiClient.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.Scanner;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
@@ -32,5 +37,18 @@ public class MultiClient {
 
         executor.shutdown();
         scanner.close();
+    }
+    private static void sendRequest(String request){
+        try(Socket socket = new Socket("localhost", PORT);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true)){
+
+            out.println(request);
+            String result = in.readLine();
+            System.out.println("Request: " + request + " => Result: " + result);
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
