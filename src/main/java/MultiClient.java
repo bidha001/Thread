50f19ce2567aff7f4b41f11ff4365e25c8c559f3
bidha001 <binitd@stud.ntnu.no>
2025-02-11 21:33:24 +0100

    added a MultiClient to send multiple operation at the same time

--- src/main/java/MultiClient.java
+++ src/main/java/MultiClient.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MultiClient {
     private static final int PORT = 5006;
@@ -10,5 +13,24 @@ public class MultiClient {
         System.out.println("How many operation do you want to send?");
         int requestCount = scanner.nextInt();
         scanner.nextLine();
+
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
+
+        for (int i = 0; i < requestCount; i++){
+            System.out.print("Enter first number: ");
+            int num1 = scanner.nextInt();
+
+            System.out.print("Enter second number: ");
+            int num2 = scanner.nextInt();
+
+            System.out.print("Enter operation (A = addition, S = subtraction, M = Multiplication, D = division): ");
+            char operator = scanner.next().charAt(0);
+            scanner.nextInt();
+
+            String request = num1 + " " + num2 + " " + operator;
+        }
+
+        executor.shutdown();
+        scanner.close();
     }
 }
